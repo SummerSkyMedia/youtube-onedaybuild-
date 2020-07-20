be547286d8b3c5bb2ee559ed28497c4056e709b6
Natalie Marshall <nataliemarshall@Natalies-iMac.attlocal.net>
2020-07-19 22:10:56 -0500

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Natalie Marshall on 7/19/20.
+//  Copyright © 2020 Natalie Marshall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
